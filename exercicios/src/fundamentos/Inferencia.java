65a07266e40beec9c1163af7513ce320332625aa
Fernanda Siqueira <nandasiq@gmail.com>
2022-09-27 19:36:13 -0300

    ainda commit do fundamentos

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,19 @@
+package fundamentos;
+
+public class Inferencia {
+
+	public static void main(String[] args) {
+		
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = 5.4;
+		System.out.println(b);
+		
+		var c = "Aqui e uma string";
+		System.out.println(c);
+		
+		c = "Mudando o string";
+		System.out.println(c);
+	}
+}
