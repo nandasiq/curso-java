bb3315e0b99cec227fd320b840219efe368ee62c
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-09 13:55:57 -0300

    aula while 1

---
+++ exercicios/src/controle/WhileDeterminado.java
@@ -0,0 +1,15 @@
+package controle;
+
+public class WhileDeterminado {
+    
+    public static void main(String[] args) {
+     
+        int i = 1;
+        while(i <= 10) {
+             System.out.println("Bora meditar");
+             i++;
+        }
+       
+    }
+
+}
