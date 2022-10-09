2954640aeb647257a3c953844d4dff02ab95ade9
Fernanda Siqueira <nandasiq@gmail.com>
2022-10-09 14:03:30 -0300

    aula for 1

---
+++ exercicios/src/controle/For1.java
@@ -0,0 +1,20 @@
+package controle;
+
+public class For1 {
+
+    public static void main(String[] args) {
+        
+        int i = 1;
+        while(i <= 10) {
+             System.out.printf("While: %d\n", i);
+             i++;
+        }
+        
+        for(int x = 0; x <= 10; x++) {
+            System.out.printf("for: %d\n", x);
+        }
+        
+        
+       
+    }
+}
