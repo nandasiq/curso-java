028ef4fefebb8143b5d8affca6c3e2b11838a300
Fernanda Siqueira <nandasiq@gmail.com>
2022-11-01 11:57:59 -0300

    Modulo OO. Alua relacao muitos para muitos

---
+++ exercicios/src/oo/composicao/CursoTeste.java
@@ -0,0 +1,43 @@
+package oo.composicao;
+
+public class CursoTeste {
+    
+    public static void main(String[] args) {
+        
+    
+    Aluno aluno1 = new Aluno("Joao");
+    Aluno aluno2 = new Aluno("Maria");
+    Aluno aluno3 = new Aluno("Pedro");
+
+    Curso curso1 = new Curso("Java");
+    Curso curso2 = new Curso("React");
+    Curso curso3 = new Curso("Security");
+    
+    curso1.adicionarAluno(aluno1);
+    curso1.adicionarAluno(aluno2);
+
+    curso2.adicionarAluno(aluno1);
+    curso2.adicionarAluno(aluno3);
+
+
+    aluno1.adicionarCurso(curso3);
+    aluno2.adicionarCurso(curso3);
+    aluno3.adicionarCurso(curso3);
+    
+    for(Aluno aluno: curso3.alunos) {
+        System.out.printf("Estou matriculado no %s", curso3.nome);
+        System.out.printf(" e meu nome e %s.\n",  aluno.nome);
+    }
+    
+    System.out.println(aluno1.cursos.get(0).alunos);
+    
+    Curso cursoEncontrado = aluno1.obterCursoPorNome("Security");
+    
+    if(cursoEncontrado != null) {
+        System.out.println(cursoEncontrado.nome);
+        System.out.println(cursoEncontrado.alunos);
+    }
+    
+    }
+   
+}
