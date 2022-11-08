9413d47165e914ff4390c0b31e5be2479a5141af
Fernanda Siqueira <nandasiq@gmail.com>
2022-11-08 15:02:14 -0300

    aula de classe a metodo abstrados

--- exercicios/src/oo/heranca/desafio/Esportivo.java
+++ exercicios/src/oo/heranca/desafio/Esportivo.java
@@ -2,7 +2,7 @@ package oo.heranca.desafio;
 
 public interface Esportivo {
     
-    //interface tem metodos, mas sem corpo
+    //interface tem metodos, mas sem corpo, sem implementar o metodo
     
     void ligarTurbo();
     void desligarTurbo();
--- exercicios/src/oo/polimorfismo/Jantar.java
+++ exercicios/src/oo/polimorfismo/Jantar.java
@@ -8,6 +8,9 @@ public class Jantar {
         fer.comer(new Feijao(0.2));
         fer.comer(new Sorvete(0.3));
         
+        //Comida ingrediente3 = new Comida(0.30); nao funciona pq a classe comida e abstrada
+        fer.comer(new Sorvete(0.3));        
+        
         System.out.println(fer.getPeso());
     }
 
